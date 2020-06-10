32d75209cbc4f4245521c751d61d60e8987a8627
Naomi Rubin <naomi@Camellia.local>
2020-06-10 14:09:38 -0700

    [F] Demo simple React component state

--- src/components/Process.js
+++ src/components/Process.js
@@ -1,10 +1,32 @@
 import React, { PureComponent } from 'react';
 
 export default class Process extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter(offset) {
+    this.setState({
+      counter: this.state.counter + offset
+    });
+  }
+
   render() {
     return(
-      <div className="Process">
+      <div className="process">
         {'Hi! This is a React Component ⚛️'}
+        <div className="counter">
+          {this.state.counter}
+          <button onClick={() => { this.incrementCounter(1) }}>
+            {'+ 1'}
+          </button>
+          <button onClick={() => { this.incrementCounter(-1) }}>
+            {'- 1'}
+          </button>
+        </div>
       </div>
     );
   }
